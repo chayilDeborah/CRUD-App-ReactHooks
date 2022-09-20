17c4789ec8f36b651f6117c68d9029eccceb2d65
chayilDeborah <aniyikayeolamide3@gmail.com>
2022-09-20 12:56:16 +0100

    create header.js file

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Contact Management App</h1>
+            <hr />
+        </header>
+    )
+}
+export default Header;
